227b1569159074b93e95d7217e2ff344e32c4385
tudorfis <tudor.fis@gmail.com>
2021-02-28 00:14:22 +0200

    commit

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,3 @@
 
 export const $ = query => document.querySelector( query )
 export const $$ = query => [ ...document.querySelectorAll( query ) ]
-
-export const ucase = str => str[0].toUpperCase() + str.substr(1)
